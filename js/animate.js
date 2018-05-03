c201ac495d78b0f968e12caef17b614644eb3c27
Fi5HBoy <me@jaykorhonen.com>
2018-05-02 20:02:47 -0500

    js scrollability

---
+++ js/animate.js
@@ -0,0 +1,8 @@
+var divs = $('.slide_show_content');
+$(window).scroll(function(){
+   if($(window).scrollTop()<10){
+         divs.stop(true,true).fadeIn("fast");
+   } else {
+         divs.stop(true,true).fadeOut("fast");
+   }
+});
